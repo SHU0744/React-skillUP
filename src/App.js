dac8ac71c89598f6430106b06d78e9e1606d7587
SHU0744 <62059397+SHU0744@users.noreply.github.com>
2021-08-22 16:59:28 +0900

    memo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./styles.css";
+import { ChildArea } from "./ChildArea";
 
 export default function App() {
+  const [text, setText] = useState("");
+  const [opne, setOpne] = useState(false);
+  const onChangeText = (e) => setText(e.target.value);
+  const onClickOpen = () => setOpne(!opne);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>非表示</button>
+      <ChildArea opne={opne} />
     </div>
   );
 }
---
+++ src/ChildArea.jsx
@@ -0,0 +1,27 @@
+import { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "100px",
+  backgroundColor: "orange"
+};
+
+// memoを使用して 親のコンポーネントが更新されても自身のprposが更新されない限りレンダリングさせない
+export const ChildArea = memo((prpos) => {
+  const { opne } = prpos;
+
+  console.log("ChildAreaがレンダリングされました");
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("aaa");
+  });
+  return (
+    <>
+      {opne ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  );
+});
