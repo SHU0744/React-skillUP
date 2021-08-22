459062d8a6fa1aeb3e0cd6c9f27ba65efbff54c2
SHU0744 <62059397+SHU0744@users.noreply.github.com>
2021-08-22 17:15:15 +0900

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import "./styles.css";
 import { ChildArea } from "./ChildArea";
 
@@ -7,14 +7,15 @@ export default function App() {
   const [opne, setOpne] = useState(false);
   const onChangeText = (e) => setText(e.target.value);
   const onClickOpen = () => setOpne(!opne);
-
+  // useCallbackで再レンダリングさせない
+  const onClickClose = useCallback(() => setOpne(false), [setOpne]);
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>非表示</button>
-      <ChildArea opne={opne} />
+      <ChildArea opne={opne} onClickClose={onClickClose} />
     </div>
   );
 }
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -6,9 +6,9 @@ const style = {
   backgroundColor: "orange"
 };
 
-// memoを使用して 親のコンポーネントが更新されても自身のprposが更新されない限りレンダリングさせない
+// useMemoを使用して 親のコンポーネントが更新されても自身のprposが更新されない限りレンダリングさせない
 export const ChildArea = memo((prpos) => {
-  const { opne } = prpos;
+  const { opne, onClickClose } = prpos;
 
   console.log("ChildAreaがレンダリングされました");
   const data = [...Array(2000).keys()];
@@ -20,6 +20,7 @@ export const ChildArea = memo((prpos) => {
       {opne ? (
         <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
