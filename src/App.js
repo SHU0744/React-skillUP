18b2abda3f25344590cb5e657021854a506d2ab8
SHU0744 <62059397+SHU0744@users.noreply.github.com>
2021-10-10 08:21:33 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import { useState, useCallback, useMemo } from "react";
 import "./styles.css";
-import { ChildArea } from "./ChildArea";
 
 export default function App() {
-  const [text, setText] = useState("");
-  const [opne, setOpne] = useState(false);
-  const onChangeText = (e) => setText(e.target.value);
-  const onClickOpen = () => setOpne(!opne);
-  // useCallbackで再レンダリングさせない
-  const onClickClose = useCallback(() => setOpne(false), [setOpne]);
-
-  //変数用の最適化
-  const temp = useMemo(() => 1 + 3, []);
-  console.log(temp);
   return (
     <div className="App">
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickOpen}>非表示</button>
-      <ChildArea opne={opne} onClickClose={onClickClose} />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
